edc5d58f4fe19125baf9f5402137d3e5c186c659
1349636970 <a1349636970@gmail.com>
2021-12-13 02:03:55 -0800

    fix bugs

--- train_ssd.py
+++ train_ssd.py
@@ -377,10 +377,10 @@ if __name__ == '__main__':
     logging.info(f"Start training from epoch {last_epoch + 1}.")
     
     for epoch in range(last_epoch + 1, args.num_epochs):
-        scheduler.step()
+        
         train(train_loader, net, criterion, optimizer,
               device=DEVICE, debug_steps=args.debug_steps, epoch=epoch)
-        
+        scheduler.step()
         if epoch % args.validation_epochs == 0 or epoch == args.num_epochs - 1:
             val_loss, val_regression_loss, val_classification_loss = test(val_loader, net, criterion, DEVICE)
             logging.info(
